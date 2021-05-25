3ab6df10557729429889ae0a5808912277434883
LHJ Piper <2940596912@qq.com>
2021-05-25 17:05:45 +0800

    test build.ps1 and deploy single page for talks/

---
+++ README.md
@@ -0,0 +1,9 @@
+# 如何使用 slidev
+部分演讲投影将基于 h5 部署，使用 [slidev](https://sli.dev/) 生成。
+
+运行 `ps` 脚本，即可生成新 `slidev` 单页面到 `talks/` 中，并且备份 `slidev` 中 `md` 配置：
+```bash
+./build.ps <演讲名称>
+```
+
+在 GitHub 或者 Gitee 配置时，将 ./talk/ 配置为 page 。
---
+++ build.ps1
@@ -0,0 +1,17 @@
+# 接收参数（演讲名称）
+param($a)
+if ([String]::IsNullOrEmpty($a)) {
+    $a = Get-Date -Format 'yyyyMMdd'
+}
+
+# build slidev
+Set-Location .\slidev
+npm run build -- --base="/$a/"
+
+# move to setup log
+Copy-Item ./dist ../talks/$a -Recurse
+
+# backup slidev.md
+Copy-Item ./slides.md ../slidev_backup/$a.md
+
+Set-Location ..
