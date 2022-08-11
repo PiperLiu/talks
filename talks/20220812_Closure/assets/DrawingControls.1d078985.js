e2a0700300cf5f741cf266f204731574ef827f45
LHJ Piper <2940596912@qq.com>
2022-08-12 01:33:47 +0800

    try 20220812 01

---
+++ talks/20220812_Closure/assets/DrawingControls.1d078985.js
@@ -0,0 +1,1 @@
+import"./DrawingControls.vue_vue_type_script_setup_true_lang.8d7ea742.js";import{_ as t}from"./DrawingControls.vue_vue_type_script_setup_true_lang.8d7ea742.js";import"./index.8dfe271d.js";export{t as default};
---
+++ talks/20220812_Closure/assets/DrawingPreview.172b1bd0.js
@@ -0,0 +1,1 @@
+import"./index.8dfe271d.js";import{ah as r}from"./index.8dfe271d.js";export{r as default};
